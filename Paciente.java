750ff0e16831521da94e5ac4ae5b072a2997ddc3
frnndds7 <frnndds7@gmail.com>
2026-07-05 13:57:11 -0400

    Update Paciente.java

--- Paciente.java
+++ Paciente.java
@@ -11,23 +11,11 @@ public class Paciente {
         this.edad = edad;
         this.servicio = servicio;
         this.derivacion = derivacion;
-        this.cronico = cronico;
-    }
-    public String getNombre(){
-        return nombre;
-    }
-    public String getComuna(){
-        return comuna;
-    }
-    public int getEdad(){
-        return edad;
-    }
-    public int getServicio(){
-        return servicio;
-    }
-    public int getDerivacion(){
-        return derivacion;
-    }
-    public int getCronico(){
-        return cronico;}
+        this.cronico = cronico;}
+    public String getNombre(){return nombre;}
+    public String getComuna(){return comuna;}
+    public int getEdad(){return edad;}
+    public int getServicio(){return servicio;}
+    public int getDerivacion(){return derivacion;}
+    public int getCronico(){return cronico;}
 }
